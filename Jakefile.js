fb2872dfddb8b6c09a56d0ed4318ecd869d08447
Andrew Smith <andrew.caleb.smith@gmail.com>
2015-04-18 18:18:20 -0700

    Use Jake instead of Make + a lot more!
    * JS linting with nodelint
    * Completed server that works with any path,
      automatically runs livereload, and watches for
      changes in src/ (dev mode only).
    * Minify CSS and JS by running $ jake env=production
    * Run $ jake server env=production to serve from
      public/ without any bells or whistles
    * Customizable BUILD and PUBLIC directory names

---
+++ Jakefile.js
@@ -0,0 +1,140 @@
+////////////////////////////////////////////////////////////////////////////////
+// JAKEFILE ////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+var express    = require('express'),
+    livereload = require('livereload'),
+    fs         = require('fs');
+
+// Constants
+var PORT   = 8080,
+    BUILD  = 'build', // Customizable BUILD and PUBLIC directory names
+    PUBLIC = 'public';
+
+// Custom functions
+jake.npmExec = function (input, opts) {
+  opts = opts || { breakOnError: true };
+  return this.exec(['PATH=$(npm bin):$PATH '+input], opts);
+}
+
+isDev = function () { return process.env.env !== 'production'; }
+isProduction = function () { return !isDev(); }
+target = function () { return isDev() ? BUILD : PUBLIC; }
+
+// TASKS ///////////////////////////////////////////////////////////////////////
+
+task('default', ['build:all']);
+
+directory(BUILD);  // Automatically creates BUILD directory when invoked
+directory(PUBLIC); // The same, but for PUBLIC
+
+desc('Runs JS through nodelint');
+task('lint', function () {
+  console.log('Running nodelint...');
+  jake.npmExec('nodelint src/script/', { breakOnError: true, printStdout: true });
+});
+
+namespace('build', function () {
+  desc('Packages all scripts into bundle.js');
+  task('script', [BUILD, 'lint'], function () {
+    console.log('Processing scripts...');
+    var debug = isDev() ? ' --debug' : '';
+    jake.npmExec(['browserify -t debowerify', debug,
+      'src/**/*.js -o '+BUILD+'/bundle.js'].join(' '));
+  });
+
+  desc('Packages all styles into bundle.css');
+  task('style', [BUILD], function () {
+    console.log('Processing styles...');
+    jake.npmExec('stylus src/style --out '+BUILD+'/bundle.css');
+  });
+
+  [BUILD, PUBLIC].forEach(function (dir) {
+    file(dir+'/index.html', ['src/index.html', dir], function () {
+      console.log('Copying index.html...');
+      jake.cpR('src/index.html', dir+'/index.html');
+    });
+  });
+
+  desc('Copies src/index.html to the target');
+  task('index', function () {
+    jake.Task['build:'+target()+'/index.html'].invoke();
+  });
+
+  desc('Performs all build tasks');
+  task('all', ['index', 'script', 'style'], function () {
+    if (isDev()) return;
+    // Minify JS and CSS
+    ['css', 'js'].forEach(function (ext) {
+      var output = PUBLIC+'/bundle.'+ext, input = BUILD+'/bundle.'+ext;
+      jake.npmExec('minify --output '+output+' '+input);
+    });
+  });
+});
+
+desc('Watch src/ and rebuild when something changes');
+task('watch', function () {
+  console.log('Watching src/');
+  jake.npmExec('watchy -w src/ -- jake', { breakOnError: true });
+});
+
+// SERVER //////////////////////////////////////////////////////////////////////
+
+// jake server port=... specifies a custom port
+// jake server env=production serves from public/ and disables livereload
+
+desc('Simple web server with livereload and src/ watching');
+task('server', ['build:all'], function () {
+  // This task implements a simple Express server which serves static files
+  // Any unknown paths will display index.html
+  //
+  // If environment is dev (default), a livereload server will be started
+  // and the livereload.js file will be injected before the </head> tag
+  // when index.html is being served
+  //
+  // If changes are noticed in src/, the project will be re-built (dev env only)
+
+  var app = express(),
+      port = process.env.port ? parseInt(process.env.port, 10) : PORT,
+      dev  = isDev(),
+      dir  = __dirname+'/'+(dev ? BUILD : PUBLIC);
+
+  if (dev) {
+    var lr = livereload.createServer();
+    lr.watch(dir);
+    console.log('Livereload running');
+    jake.Task['watch'].invoke();
+  }
+
+  function showIndex(res, next) {
+    // Read index.html and inject livereload stuff into it
+    fs.readFile(dir+'/index.html', function (err, data) {
+      if (!data) { next() ; return };
+      var html = data.toString(), inj;
+
+      if (dev) {
+        inj = '<script src="http://localhost:35729/livereload.js"></script>';
+        html = data.toString().replace(/(<\/head>)/, inj+'\1');
+      }
+
+      res.format({'text/html': function () { res.send(html); } });
+    });
+  }
+
+  app.use(function(req, res, next) {
+    console.log(req.method+' '+req.path);
+    if (!req.path.match(/^index\.html|^\/$/)) {
+      fs.open(dir+req.path, 'r', function (err) {
+        // No error? This is an asset, pass to other routes
+        err ? showIndex(res, next) : next();
+      });
+    } else {
+      showIndex(res, next);
+    }
+  });
+
+  app.use(express.static(dir));
+
+  var server = app.listen(port);
+  console.log('Server running on localhost:'+port);
+});
--- README.markdown
+++ README.markdown
@@ -4,19 +4,23 @@
 
 This is a basic one-page JavaScript project which uses `bower` and `npm` for
 package management, `stylus` for CSS, `browserify` for JavaScript dependency
-management, and `make` for building and tasks (instead of `gulp` or `grunt`).
+management, and `jake` for building and tasks (instead of `gulp` or `grunt`).
 
 ## Getting started
 
  1. Install `node` at `https://nodejs.org/download/`. Also consider using `nvm`
     to manage different versions of `node`
  1. `$ npm install`
- 1. `$ make` to build the project. This will install bower components
-    if they aren't already installed
- 1. `$ make server` to start a server. Browsers connected to the server will
+ 1. `$ npm install -g jake` to install Jake globally
+ 1. `$ npm install -g bower` to install Bower globally
+ 1. `$ bower install` to install Bower components
+ 1. `$ jake` to build the project
+ 1. `$ jake server` to start a server. Browsers connected to the server will
     automatically refresh if anything in `build/` changes
- 1. `$ make watch` to watch for changes in `src/` and re-make them, causing
-    `build/` to change, and the server to prompt browsers to refresh, if running
+ 1. Run Jake with `env=production` to build and minify source files
+    and send them to the `public/` directory (e.g. `$ jake env=production`),
+    or to serve the `public/` directory without livereload or source watching
+    (e.g. `$ jake server env=production`)
 
 ## Adding packages
 
@@ -30,11 +34,5 @@ management, and `make` for building and tasks (instead of `gulp` or `grunt`).
 
 ## TODO
 
- * Use Jake instead of Make to eliminate the need for cryptic shell commands?
- * Use the `debowerify` transform for `browserify` or make a script that
-   automatically symlinks JS files from bower components into `script/lib/`
  * Add test suite manager
- * Automatic `$ bower install` should check if all `bower` dependencies are
-   fulfilled, not just for the existence of `bower_components/`
  * Make this into a `yeoman` recipe, or whatever those are called?
- * Make sure the dev server maps all paths to `build/index.html`
